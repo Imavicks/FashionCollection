bef5fdaab1eb09d8e4adc969e2bca318dbcc6a1b
Imavicks <victor.imazobi@gmail.com>
2023-09-22 14:50:58 +0100

    create a popup page for login/signup

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// function to open dashboard menu
 function openNav() {
     document.getElementById("mySidebar").style.width = "200px";
     document.getElementById("main").style.display = "none";
 }
-  
+
+// function to  close dashboard menu
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.display = "block";
 }
+
