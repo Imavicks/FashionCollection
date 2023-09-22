ed9a89638a64383d2b25b2a407d614e357d31e3a
Imavicks <victor.imazobi@gmail.com>
2023-09-22 14:52:59 +0100

    add javascript logic to the popup page

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function closeNav() {
     document.getElementById("main").style.display = "block";
 }
 
+// function to open login popup
+document.querySelector('.login').addEventListener('click', function() {
+    document.getElementById('popup-container').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+});
+
+// function to close login popup
+function closePopup() {
+    document.getElementById('popup-container').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+document.getElementById('overlay').addEventListener('click', closePopup);
+
+document.querySelector('.close-button').addEventListener('click', closePopup);
+
+// get user's inputs
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    closePopup();
+});
